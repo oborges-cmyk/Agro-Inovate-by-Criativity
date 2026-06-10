11e8fe5ecb778e2f12a28895a0c584dde4067174
isaraujou <o.borges@escola.pr.gov.br>
2026-06-10 11:00:18 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+let carrinho = ['Mudas de tomate', 'Sementes de milho','Mudas de café'];
+
 function comprar(produto){
     alert("Você escolheu: " + produto);
     carrinho.push(produto);
 }
-
-let carrinho = ['Mudas de tomate', 'Sementes de milho','Mudas de café'];
